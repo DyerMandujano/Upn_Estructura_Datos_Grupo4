321f834f66d2ea66b5f1e75f905de33d775d8448
DyerMandujano <dyer0206@gmail.com>
2024-04-26 21:14:14 -0500

    Agregando clase Materiales y lista Materiales

---
+++ PryHoteleria_Upn_Grupo4/ListaMateriales.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PryHoteleria_Upn_Grupo4
+{
+    internal class ListaMateriales
+
+    {
+        //Definimos el nodo de tipo Material
+        private Materiales lista_mat;
+
+        //Propiedad Lista_cliente para acceder a la lista_mat
+        public Materiales Lista_material
+        {
+            get { return lista_mat; }
+            set { lista_mat = value; }
+        }
+
+        //Definimos el constructor e inicializamos la listaclientes
+        public ListaMateriales()
+        {
+            //la propiedad 'Lista_mateiral' será null
+            Lista_material = null;
+        }
+
+        //Metodo para determinar si la lista esta vacia
+        public bool VerificarListaVacia()
+        {
+            if (Lista_material == null)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public void InsertarMaterialInicio(int id, string nombre_material, int cantidad, double costo)
+        {
+            //Creamos el nodo material a ingresar
+            Materiales q = new Materiales(id, nombre_material, cantidad, costo);
+            if (Lista_material == null)
+            {
+                //La lista apuntará al objeto 'q' que estamos agregando.
+                Lista_material = q;
+            }
+            else
+            {
+                //El valor ingresado a punta a la lista que ya se encontraba
+                q.MT_Sgte = Lista_material;
+                //La lista apunta al 'q' para q este sea el nuevo valor inicial
+                Lista_material = q;
+            }
+        }
+
+
+        public void InsertarMaterialAlFinal(int id, string nombre_material, int cantidad, double costo)
+        {
+            //Creamos el objeto que se ingresará al final de la lista
+            Materiales q = new Materiales(id, nombre_material, cantidad, costo);
+            //Si la lista es Null
+            if (Lista_material == null)
+            {
+                //La lista apuntará al objeto 'q' que estamos agregando.
+                Lista_material = q;
+            }
+            //DE LO CONTRARIO, SI HAY ELEMENTOS EN LA LISTA
+            else
+            {
+                //Esta variable 'p' se utilizará para recorrer la lista hasta encontrar el último nodo.
+                //Por lo que a 'p' le asignamos todos los valores que se encuentra en la Lista_material
+                Materiales p = Lista_material;
+                //Mientras la propiedad Cl_Sgte sea distinto de null
+                while (p.MT_Sgte != null)
+                {
+                    //Avanzará hasta el ultimo nodo
+                    p = p.MT_Sgte;
+                }
+                //UNA VEZ ENCONTRADO EL ULTIMO NODO
+                //La propiedad Cl_Sgte apuntará al nuevo Cliente ingresado
+                p.MT_Sgte = q;
+            }
+        }
+
+        public void EliminarMaterialInicio()
+        {
+            if (VerificarListaVacia())
+            {
+                Console.WriteLine("LA LISTA ESTÁ VACIA!!");
+                return;
+            }
+            else
+                Lista_material = Lista_material.MT_Sgte;
+            Console.WriteLine("\nMATERIAL INICIAL ELIMINADO!");
+        }
+
+
+        public void EliminarMaterialFinal()
+        {
+            if (VerificarListaVacia())
+            {
+                Console.WriteLine("LA LISTA ESTÁ VACIA!!");
+                return;
+            }
+            else
+            {
+                //CONSIDERANDO QUE SOLO HAYA UN NODO EN LA LISTA
+                //SI LA DIRECCION DEL NODO ES NULL
+                if (Lista_material.MT_Sgte == null)
+                {
+                    //LA LISTA APUNTARA A UNA DIRECCION NULA LO QUE CONLLEVA A LA ELIMINACION DEL UNICO NODO
+                    Lista_material = Lista_material.MT_Sgte;
+                }
+                else
+                {
+                    //EL 'p' NOS SERVIRA PARA RECORRER LA LISTA
+                    Materiales p = Lista_material;
+                    //El 'nd_ant' NOS SERVIRA PARA ALMACENAR LOS VALORES DEL 'p' MIENTRAS ESTE RECORRIENDO LA LISTA
+                    Materiales nd_ant = new Materiales();
+                    //MIENTRAS QUE LA DIRECCION DEL objeto 'p' SEA DISTINTO DE NULL 
+                    while (p.MT_Sgte != null)
+                    {
+                        //LOS DATOS DEL OBJETO 'p' SE ALMACENARÁN EN EL OBJETO 'nd_anterior'
+                        nd_ant = p;
+                        //Y EL OBJETO 'p' IRA AL SIGUIENTE NODO DE LA LISTA
+                        p = p.MT_Sgte;
+                    }
+                    //UNA VEZ HAYA LLEGADO AL ULTIMO NODO, COMO EL Objeto 'nd_anterior' guardo el penultimo nodo del objeto 'p',
+                    //EL OBJETO 'nd_anterior' APUNTARA AL NULL YA QUE AHORA ESE SE CONVERTIRA EN EL ULTIMO NODO, PORQUE EL OTRO HA SIDO ELMINADO
+                    nd_ant.MT_Sgte = null;
+                }
+                Console.WriteLine("\nMATERIAL FINAL ELIMINADO!");
+            }
+        }
+
+        public void EliminarMaterialPorID(int id)
+        {
+            if (VerificarListaVacia())
+            {
+                Console.WriteLine("LA LISTA ESTÁ VACIA!!");
+                return;
+            }
+            bool encontrado = false;
+            Materiales p = Lista_material;
+            Materiales ant = new Materiales();
+
+            while (p != null)
+            {
+                //Buscar el valor del nodo en la lista mediante el dato pasado por parametro
+                //Es decir, Si el 'valor' pasado por parametro es igual al numero que se encuentra en la propiedad 'Id' del nodo
+                if (p.Id == id)
+                {
+                    //Si el valor es encontrado en el primer elemento de la lista
+                    if (p == Lista_material)
+                    {
+                        //La lista va a apuntar a la direccion del siguiente nodo y por lo tanto eliminará el nodo q se estaba buscando
+                        Lista_material = Lista_material.MT_Sgte;
+                    }
+                    else
+                    {
+
+                        //Si se trata de un elemento diferente del primero
+                        //hacemos que el obj 'ant' de tipo nodo obtenga la direccion del siguiente nodo, para que el nodo con el valor encontrado se elimine
+                        //y continue con el siguiente nodo.
+                        ant.MT_Sgte = p.MT_Sgte;
+                    }
+                    Console.WriteLine("Cliente con ID " + id + " Eliminado Correctamente!");
+                    encontrado = true;
+                    break;
+                }
+                //Si todavia no encuentra el valor deseado, va a seguir estas instrucciones
+                //Guarda el elemento de 'p' en 'ant'
+                ant = p;
+                //Ir al siguiente elemento de la lista
+                p = p.MT_Sgte;
+            }
+            if (!encontrado)
+            {
+                Console.WriteLine("No se encontró ningún cliente con el ID " + id);
+            }
+
+
+
+        }
+
+        //Metodo para eliminar la lista
+        public void EliminaTodaLaLista()
+        {
+            Lista_material = null;
+            Console.WriteLine("LISTA ELIMINADA CORRECTAMENTE!");
+        }
+
+        public void BuscarMateriales(string nombre_material)
+        {
+            Materiales p = Lista_material;
+            //Mientras el 'p' sea diferente de null y su valor sea distinto al que se esta pasando por parametro
+            //Va a recorrer la lista hasta encontrar el valor deseado
+            while ((p != null) && (p.NombreMaterial != nombre_material))
+            {
+                //Imprime el valor q tiene el 'p'
+                Console.WriteLine(p.NombreMaterial);
+                //p apunta al siguiente nodo
+                p = p.MT_Sgte;
+            }
+            //SI YA RECORRIO TODA LA LISTA, entonces entra a este 'if'
+            //Por lo tanto, cuando llega al ultimo elemento el p va a apuntar al null
+            //Es por ello que si 'p' es null
+            if (p == null)
+                //el elemento buscado no se encuentra en la lista
+                Console.WriteLine("El ID " + nombre_material + " no se encuentra en la lista");
+            //De lo contrario, si esta.
+            else
+            {
+                Console.WriteLine("Datos del Material");
+                Console.WriteLine("******************************");
+                Console.WriteLine(p.Id + "->" + p.NombreMaterial + "->" + p.Cantidad + "->" + p.Costo);
+
+            }
+        }
+
+
+        public void ImprimirMaterial()
+        {
+            Materiales p = Lista_material;
+            if (VerificarListaVacia())
+            {
+                Console.WriteLine("LA LISTA ESTÁ VACIA!!");
+                return;
+            }
+            //Recorrer la lista
+            Console.WriteLine("Lista Enlazada con Datos");
+            Console.WriteLine("*************************");
+            Console.Write("Lista -> ");
+            while (p != null)
+            {
+                Console.WriteLine(p.Id + "->" + p.NombreMaterial + "->" + p.Cantidad + "->" + p.Costo);
+                p = p.MT_Sgte;
+            }
+            Console.WriteLine("NULL ");
+
+
+        }
+    }
+}
---
+++ PryHoteleria_Upn_Grupo4/Materiales.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PryHoteleria_Upn_Grupo4
+{
+    internal class Materiales
+    {
+        //ATRIBUTOS
+        private int id;
+        private string nombre_material;
+        private int cantidad;
+        private double costo;
+        private Materiales mt_sgt;
+
+        public Materiales()
+        {
+
+        }
+
+        public Materiales(int ID, string nombre_material, int cantidad, double costo)
+        {
+            //MATERIALES DEL PRODUCTO
+            Id = ID;
+            NombreMaterial = nombre_material;
+            Cantidad = cantidad;
+            Costo = costo;
+            //Inicializar el enlace en null
+            MT_Sgte = null;
+        }
+
+        //Propiedad para acceso al enlace siguiente
+        public Materiales MT_Sgte
+        {
+            get { return mt_sgt; } //Leer el enlace siguiente
+            set { mt_sgt = value; } //Escribir enlace siguiente
+        }
+
+        //GETTERS Y SETTERS DE LAS PROPIEDADES
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string NombreMaterial
+        {
+            get { return nombre_material; }
+            set { nombre_material = value; }
+        }
+
+        public int Cantidad
+        {
+            get { return cantidad; }
+            set { cantidad = value; }
+        }
+
+        public double Costo
+        {
+            get { return costo; }
+            set { costo = value; }
+        }
+    }
+}
