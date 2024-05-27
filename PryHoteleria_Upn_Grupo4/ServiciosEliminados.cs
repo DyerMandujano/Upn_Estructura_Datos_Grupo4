63d7e1c31e467ac53c32a880f312813fbae15066
Rouge3113 <marcos.zapatalopez@gmail.com>
2024-05-26 23:32:57 -0500

    He añadido las pilas en los Servicios

---
+++ PryHoteleria_Upn_Grupo4/ServiciosEliminados.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PryHoteleria_Upn_Grupo4
+{
+    public  class ServiciosEliminados
+    {
+        //Cola
+        NodoServicioEliminado _inicio;
+
+        public void Enqueue(NodoServicioEliminado unServicio)
+        {
+            if (_inicio == null)
+            {
+                _inicio = unServicio;
+            }
+            else
+            {
+                NodoServicioEliminado aux = BuscarUltimo(_inicio);
+                aux.Siguiente = unServicio;
+            }
+        }
+
+        private NodoServicioEliminado BuscarUltimo(NodoServicioEliminado unServicio)
+        {
+            if (unServicio.Siguiente == null)
+            {
+                return unServicio;
+            }
+            else 
+            {
+                return BuscarUltimo(unServicio.Siguiente);
+            }
+        }
+
+        public void Dequeue()
+        {
+            _inicio = _inicio.Siguiente;
+        }
+
+        public NodoServicioEliminado Peek
+        {
+            get {  return _inicio; }
+        }
+    }
+}
