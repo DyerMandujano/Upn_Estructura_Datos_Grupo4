dd1f63d96f52a6a4b71d56a23c91b4b2b5f13354
lionelnoob <lionelsalvatier03@gmail.com>
2024-06-24 20:39:41 -0500

    creando clase atencion cliente

---
+++ PryHoteleria_Upn_Grupo4/Atencion_Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PryHoteleria_Upn_Grupo4
+{
+    internal class Atencion_Cliente
+    {
+
+    }
+}
