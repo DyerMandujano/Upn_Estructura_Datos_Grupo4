9b917dcb489ee5ccfeaa1f1f8a9b68f2a14502b6
Rouge3113 <marcos.zapatalopez@gmail.com>
2024-07-08 21:19:01 -0500

    se agrego el check out

---
+++ PryHoteleria_Upn_Grupo4/ArbolCheckOut/NodoCheckOut.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PryHoteleria_Upn_Grupo4.ArbolCheckOut
+{
+    internal class NodoCheckOut
+    {
+    }
+}
